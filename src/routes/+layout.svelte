0234b9d052d952ae09be3692b236b49a059b9883
Naous Islam <naous.islam@outlook.com>
2024-02-20 16:02:52 -0800

    head fix.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
     import Navigation from "$lib/components/navigation.svelte";
-	import Analytics from "$lib/components/analytics.svelte";
     import "$lib/scss/styles.scss"
 </script>
 
-<Analytics></Analytics>
+<svelte:head>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-J756NXPL9Q');
+    </script>
+</svelte:head>
+
 <Navigation></Navigation>
 <slot />
