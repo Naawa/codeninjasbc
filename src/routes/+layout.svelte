70d652e187eb7e90181fd30fcd16eb0e48a64e50
Naous Islam <naous.islam@outlook.com>
2024-02-20 16:23:23 -0800

    New analytics.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,9 @@
 <script lang="ts">
-    import Navigation from "$lib/components/navigation.svelte";
+    import Analytics from "$lib/components/analytics.svelte";
+import Navigation from "$lib/components/navigation.svelte";
     import "$lib/scss/styles.scss"
 </script>
 
-<svelte:head>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-12HD95QXZJ"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-12HD95QXZJ');
-    </script>
-</svelte:head>
-
+<Analytics></Analytics>
 <Navigation></Navigation>
 <slot />
