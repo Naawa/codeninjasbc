947c8a65852a05967b700dfbfc0deb0bff86a920
Naous Islam <naous.islam@outlook.com>
2024-03-03 18:07:39 -0800

    Added background.

---
+++ src/lib/components/background.svelte
@@ -0,0 +1,12 @@
+<img src="/images/landingbg.png" alt="Background">
+
+<style lang="scss">
+    img {
+        aspect-ratio: 16/9;
+        position: fixed;
+        top: 0;
+        width: 100dvw;
+        top: 0;
+        z-index: -1;
+    }
+</style>
