e9a2c921e3ffcd521f5e01459166300b6a1c47c8
Naous Islam <naous.dev@outlook.com>
2024-04-17 12:10:56 -0700

    Fixed height of section.

--- src/lib/components/landing/Junior.svelte
+++ src/lib/components/landing/Junior.svelte
@@ -96,7 +96,7 @@
     @media (width < 1200px) {
         section {
             flex-direction: column;
-            padding: 4em 1em 3em;
+            padding: 4em 1em 1em;
             gap: 1em;
 
             div {
