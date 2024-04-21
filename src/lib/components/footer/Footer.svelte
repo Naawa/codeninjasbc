e70ea528bc867d51364da86effa76efd253e8281
Naous Islam <naous.dev@outlook.com>
2024-04-20 18:24:00 -0700

    Added footer.

---
+++ src/lib/components/footer/Footer.svelte
@@ -0,0 +1,29 @@
+<section>
+    <h4>STORE HOURS</h4>
+    <span>
+        <p>Monday - Friday</p>
+        <h5>3:30 - 7:30 PM</h5>
+    </span>
+    <span>
+        <p>Saturday</p>
+        <h5>10:00 - 2:00 PM</h5>
+    </span>
+</section>
+
+<style lang="scss">
+    section {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        gap: 1em;
+        min-height: 40vh;
+        background-color: #04325a;
+        padding: 4em;
+
+        h4 {
+            color: #f6f7f8;
+        }
+    }
+</style>
--- src/lib/components/inquiry/InquiryForm.svelte
+++ src/lib/components/inquiry/InquiryForm.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-	import { goto } from "$app/navigation";
 	import { page } from "$app/stores";
-	import { dojos, locations } from "$lib/utils/dojos";
+	import { dojos } from "$lib/utils/dojos";
 	import { superForm } from "sveltekit-superforms";
 
 
