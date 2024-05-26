e913fc8ddd64ba1484b82fe2b7457e172cba1ea0
Naous Islam <naous.islam@outlook.com>
2024-05-26 09:34:45 -0700

    Updated tsconfig.

--- src/lib/db/supabaseClient.ts
+++ src/lib/db/supabaseClient.ts
@@ -1,5 +1,5 @@
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
+import { PUBLIC_SUPABSE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from './supabase'
 
-export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
+export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABSE_ANON_KEY)
