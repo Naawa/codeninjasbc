ce5eceb8b8c70ee090ac9e638646da10775766a3
Naous Islam <naous.islam@outlook.com>
2024-09-17 12:47:13 -0700

    Reverted date filtering.

--- src/lib/components/forms/tour/AvailableTourDateSelector.svelte
+++ src/lib/components/forms/tour/AvailableTourDateSelector.svelte
@@ -61,7 +61,7 @@
         </div>
         <span>
             {#each availableTourDates as tourDate,  i}
-                {#if new Date(tourDate.date).toLocaleDateString() != "Invalid Date" && tourDate.slots.length > 0}
+                {#if new Date(tourDate.date).toLocaleDateString() != "Invalid Date"}
                         <div>
                             <h5>{new Date(tourDate.date).toLocaleDateString(undefined, {
                                 weekday: "long",
